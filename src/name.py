09f98169dad8f70b0570e300f51058746b7c7f3c
ilya <ilifud98@mail.ru>
2025-04-20 01:30:01 +0700

    Организация проекта

---
+++ src/name.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+
+def greeting(datetime: datetime):
+    if 6 < datetime.time().hour < 12:
+        return "Доброе утро"
+    elif 12 <= datetime.time().hour < 18:
+        return "Добрый день"
+    elif 18 <= datetime.time().hour < 22:
+        return "Добрый вечер"
+    else:
+        return "Добрый ночи"
+
+if __name__ == "__main__":
+    time_now = datetime.now()
+    print(greeting(time_now))
